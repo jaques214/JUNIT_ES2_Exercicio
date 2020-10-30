228205fd8a6be1ef602e0b133b6237c26fd39c30
Jaques Resende <8190214@estg.ipp.pt>
2020-10-30 11:47:52 +0000

    code correction

--- src/main/java/BinString.java
+++ src/main/java/BinString.java
@@ -6,14 +6,14 @@ public class BinString{
     }
 
     public int sum(String s){
-        if(s == "") return 0;
+        if(s.equals("")) return 0;
         if (s.length() == 1) return ((int) s.charAt(0));
         return (((int) s.charAt(0))) + sum(s.substring(1));
     }
 
     public String binarise(int x){
         if(x==0) return "";
-        if(x%2 == 1) return "1" + binarise(x/2) ;
-        return "0" + binarise(x/2);
+        if(x%2 == 1) return binarise(x/2) + "1";
+        return binarise(x/2) + "0";
     }
 }
